5a857dae80732ee583710882cc190457f76594e4
slava1967 <fx.slav21@gmail.com>
2021-04-08 12:46:03 +0300

    First commit

---
+++ pages/contact.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <PageBanner pageTitle="Contact" />
+    <Contact />
+    <Map />
+    <ClientsTwo />
+  </div>
+</template>
+
+<script>
+  import PageBanner from "~/components/pages/PageBanner";
+  import Contact from "~/components/pages/Contact";
+  import Map from "~/components/home2/Map";
+  import ClientsTwo from "~/components/home2/ClientsTwo";
+  export default {
+    components: {
+      Contact,
+      PageBanner,
+      Map,
+      ClientsTwo,
+    },
+    head(){
+      return {
+        title: "Meipaly | Contact"
+      }
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <SliderOne />
+    <AboutAgency />
+    <BlogHome />
+    <CallToAction />
+  </div>
+</template>
+
+<script>
+  import SliderOne from "~/components/home/SliderOne";
+  import AboutAgency from "~/components/home/AboutAgency";
+  import BlogHome from "~/components/home/BlogHome";
+  import CallToAction from "~/components/home/CallToAction";
+  export default {
+    components: {
+      SliderOne,
+      AboutAgency,
+      BlogHome,
+      CallToAction,
+    }
+  }
+</script>
